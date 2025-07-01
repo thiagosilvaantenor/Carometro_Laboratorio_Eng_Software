71db2d4ed5b70eff8e6a501862e463204989baf9
Thiago Antenor <thiagoantenor31@gmail.com>
2025-07-01 17:48:34 -0300

    feat: add página de SaibaMais

--- src/main/java/br/com/carometro/controller/InicioController.java
+++ src/main/java/br/com/carometro/controller/InicioController.java
@@ -2,10 +2,12 @@ package br.com.carometro.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
+@RequestMapping("/")
 public class InicioController {
 
 	
@@ -13,6 +15,13 @@ public class InicioController {
     public ModelAndView paginaInicio() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("inicio/inicio");
+        return modelAndView;
+    }
+	
+	@GetMapping("/saibaMais")
+    public ModelAndView paginaSaibaMais() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("inicio/saibaMais");
         return modelAndView;
     }
 }
